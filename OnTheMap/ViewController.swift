262b0b1c03290f29a252b0165be8b28a2c238583
Michelangelo <michelangelo@MacBook-Pro-di-Michelangelo.local>
2017-01-14 11:47:21 +0100

    Initial Commit

---
+++ OnTheMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OnTheMap
+//
+//  Created by Michelangelo on 14/01/2017.
+//  Copyright © 2017 michelangelo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
